f69ff74ca72fde1b15089bd8643aee7e27fffa9e
dongjay00 <cjswodlehdgn@gmail.com>
2025-10-17 16:37:43 +0900

    feat: 라우트 그룹

--- src/app/(afterLogin)/home/layout.tsx
+++ src/app/(afterLogin)/home/layout.tsx
@@ -3,5 +3,10 @@ export default async function HomeLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      홈 레이아웃
+      {children}
+    </div>
+  );
 }
